f10aac4aa7a5ba2b1675aa9ea787ebcd635233fb
Palermo Rodriguez <nicolas.palermo@kcc.com>
2022-05-10 14:29:11 -0300

    42) Eventos en React
    Creamo un button con un evento "onClick" . Estos eventos utilizan una función para que funionen. Se creo una función "seleccionarProducto

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -11,12 +11,19 @@ import React from 'react';
 
 const Producto = ({product}) => {
 
-    const {name,price} = product
+    const {name,price,id} = product
+
+    const seleccionarProducto = (id) =>{
+        console.log(`Comprando el producto con ID: ${id}`);
+    }
 
     return(
        <div>
            <h2>Product: {name}</h2>
            <h3>Price: {price}</h3>
+           <button type='button' onClick={()=>seleccionarProducto(id)}>
+                Comprar
+           </button>
        </div>
     )
 
