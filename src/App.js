f3e2e11b2270f8a55cdbe86aee5c917753931ace
Palermo Rodriguez <nicolas.palermo@kcc.com>
2022-05-10 11:48:14 -0300

    40) Primeros pasos con el State de React
    Creamos un useState en nuestro componente principal

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 
 import Header from "./components/Header.";
 import Footer from "./components/Footer";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////
@@ -11,6 +11,13 @@ import { Fragment } from "react";
 
 
 const App = () => {
+
+  const [products, setProducts] = useState([
+    {id:1, name: "Angular JS T-shirt", price: 50},
+    {id:2, name: "Vue JS T-shirt", price: 30},
+    {id:3, name: "React JS T-shirt", price: 40},
+    {id:4, name: "Node JS T-shirt", price: 20}
+  ]);
 
   const fecha = new Date().getFullYear();
 
